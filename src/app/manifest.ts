78d418b85b349ae08fd39019f5989c9e17adb007
thenam2kx <thenam2kx@gmail.com>
2025-03-17 04:05:35 +0700

    fix error build

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,18 +5,18 @@ export default function manifest(): MetadataRoute.Manifest {
     name: "Chill beats",
     short_name: "Chill beats",
     description: "Online music player",
-    icons: [
-      {
-        src: "https://salt.tikicdn.com/ts/upload/2f/51/80/5643672027a54bfa593300f53c91c12a.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "https://salt.tikicdn.com/ts/upload/2f/51/80/5643672027a54bfa593300f53c91c12a.png",
-        sizes: "512x512",
-        type: "image/png",
-      }
-    ],
+    // icons: [
+    //   {
+    //     src: "https://salt.tikicdn.com/ts/upload/2f/51/80/5643672027a54bfa593300f53c91c12a.png",
+    //     sizes: "192x192",
+    //     type: "image/png",
+    //   },
+    //   {
+    //     src: "https://salt.tikicdn.com/ts/upload/2f/51/80/5643672027a54bfa593300f53c91c12a.png",
+    //     sizes: "512x512",
+    //     type: "image/png",
+    //   }
+    // ],
     theme_color: "#1A94FF",
     background_color: "#1A94FF",
     start_url: "/",
