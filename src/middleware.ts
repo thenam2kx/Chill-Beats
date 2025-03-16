af1cc4ae9f3547e2d9985b173b1034fab502671b
thenam2kx <thenam2kx@gmail.com>
2025-03-17 02:02:35 +0700

    protected route with middleware next-auth

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { withAuth } from 'next-auth/middleware';
+// export { default } from "next-auth/middleware"
+
+// redirect to singin page if user is not authenticated
+export default withAuth({
+  pages: {
+    signIn: '/auth/signin',
+  }
+})
+
+export const config = { matcher: ["/playlist", "/track/upload", "/like"] }
--- src/utils/auth.options.ts
+++ src/utils/auth.options.ts
@@ -5,7 +5,7 @@ import { JWT } from "next-auth/jwt"
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.NO_SECRET!,
+  secret: process.env.NEXTAUTH_SECRET!,
 
   // Configure one or more authentication providers
   providers: [
